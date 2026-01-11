a4c03ac1eb23b7faf2859cd943bf848f4b5455cb
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:34:26 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "cdn_frontdoor_custom_domain_associations" {
+  description = "All cdn_frontdoor_custom_domain_association resources"
+  value       = azurerm_cdn_frontdoor_custom_domain_association.cdn_frontdoor_custom_domain_associations
+}
+
