02efafb50301db6075fca4975ee84ee31b2bf42c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:18 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "cdn_frontdoor_custom_domain_associations" {
-  description = "All cdn_frontdoor_custom_domain_association resources"
-  value       = azurerm_cdn_frontdoor_custom_domain_association.cdn_frontdoor_custom_domain_associations
-}
 output "cdn_frontdoor_custom_domain_associations_cdn_frontdoor_custom_domain_id" {
-  description = "List of cdn_frontdoor_custom_domain_id values across all cdn_frontdoor_custom_domain_associations"
-  value       = [for k, v in azurerm_cdn_frontdoor_custom_domain_association.cdn_frontdoor_custom_domain_associations : v.cdn_frontdoor_custom_domain_id]
+  description = "Map of cdn_frontdoor_custom_domain_id values across all cdn_frontdoor_custom_domain_associations, keyed the same as var.cdn_frontdoor_custom_domain_associations"
+  value       = { for k, v in azurerm_cdn_frontdoor_custom_domain_association.cdn_frontdoor_custom_domain_associations : k => v.cdn_frontdoor_custom_domain_id }
 }
 output "cdn_frontdoor_custom_domain_associations_cdn_frontdoor_route_ids" {
-  description = "List of cdn_frontdoor_route_ids values across all cdn_frontdoor_custom_domain_associations"
-  value       = [for k, v in azurerm_cdn_frontdoor_custom_domain_association.cdn_frontdoor_custom_domain_associations : v.cdn_frontdoor_route_ids]
+  description = "Map of cdn_frontdoor_route_ids values across all cdn_frontdoor_custom_domain_associations, keyed the same as var.cdn_frontdoor_custom_domain_associations"
+  value       = { for k, v in azurerm_cdn_frontdoor_custom_domain_association.cdn_frontdoor_custom_domain_associations : k => v.cdn_frontdoor_route_ids }
 }
 
